6f24c02c3c8414643053ec4448505d1f7354906c
QiuChenly <963084062@qq.com>
2017-10-29 15:52:59 +0800

    项目立项，首次初始化数据。

---
+++ app/src/main/java/qiuchenly/JSAHVCSC/Basic/BasicApp.java
@@ -0,0 +1,10 @@
+package qiuchenly.JSAHVCSC.Basic;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chenyuqi on 2017/10/29.
+ */
+
+public class BasicApp extends AppCompatActivity {
+}
