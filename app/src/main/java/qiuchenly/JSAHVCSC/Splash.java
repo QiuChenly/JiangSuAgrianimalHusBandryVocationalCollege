f5f89e5958e5d4f094598b23a93f7929f466b373
QiuChenly <963084062@qq.com>
2017-10-30 21:09:55 +0800

    处理完界面动画

--- app/src/main/java/qiuchenly/JSAHVCSC/Splash.java
+++ app/src/main/java/qiuchenly/JSAHVCSC/Splash.java
@@ -1,29 +1,39 @@
 package qiuchenly.JSAHVCSC;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import qiuchenly.JSAHVCSC.Base.BaseApp;
 import qiuchenly.JSAHVCSC.Base.BaseUtils;
 import qiuchenly.JSAHVCSC.Base.iSetting.Sets;
 import qiuchenly.JSAHVCSC.UIViews.Login;
 
-public class Splash extends BaseApp implements Animation.AnimationListener {
+public class Splash extends BaseApp implements Animator.AnimatorListener {
     ImageView mJSAHVC;
+
+    LinearLayout mLoginBox;
 
     @Override
     public Sets getDefaultSet(Sets sets) {
-        sets.ViewID=R.layout.view_splash;
-        sets.allowStatusBarTranslate=true;
+        sets.ViewID = R.layout.view_splash;
+        sets.allowStatusBarTranslate = true;
 //        sets.doubleClickToExit=false;
-        sets.hideTitleBar=true;
-        sets.NullBack=true;
+        sets.hideTitleBar = true;
+        sets.NullBack = true;
         return sets;
     }
 
@@ -34,36 +44,62 @@ public class Splash extends BaseApp implements Animation.AnimationListener {
 
     @Override
     public void resolveFinish() {
-        Bitmap bit = BitmapFactory.decodeResource(getResources(),R.mipmap.jsahvc);
-        mJSAHVC.setImageBitmap(BaseUtils.makeRound(bit,52));
-        AnimationSet sets=new AnimationSet(true);
-        TranslateAnimation translate = new TranslateAnimation(0,0,-500,0);
-        sets.addAnimation(translate);
-        RotateAnimation rotate = new RotateAnimation(0,359+359+359+359, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        sets.addAnimation(rotate);
-        sets.setDuration(3000);
-        sets.setFillAfter(true);
-        sets.setAnimationListener(this);
-        mJSAHVC.setAnimation(sets);//设置总动画
+        Bitmap bit = BitmapFactory.decodeResource(getResources(), R.mipmap.jsahvc);
+        mJSAHVC.setImageBitmap(BaseUtils.makeRound(bit, 52));
+
+        int DEVICES_HEIGHT = getWindowManager().getDefaultDisplay().getHeight()/2;
+
+        ObjectAnimator animator_Translate = ObjectAnimator.ofFloat
+                (mJSAHVC, "TranslationY", -1 * DEVICES_HEIGHT, 0);
+        ObjectAnimator animator_Rotate = ObjectAnimator.ofFloat
+                (mJSAHVC, "rotation", 0f, 360f * 5);
+
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(animator_Translate, animator_Rotate);
+        set.setInterpolator(new OvershootInterpolator());
+        set.setDuration(3000);
+        set.addListener(this);
+        set.start();
     }
+
 
     @Override
     public void findID() {
-        mJSAHVC=find(R.id.mJSAHVCImg);
+        mJSAHVC = find(R.id.mJSAHVCImg);
+        mLoginBox = find(R.id.mLoginBox);
+    }
+
+
+    @Override
+    public void onAnimationStart(Animator animator) {
     }
 
     @Override
-    public void onAnimationStart(Animation animation) {
+    public void onAnimationEnd(Animator animator) {
+        int BOX_HEIGHT;
+        BOX_HEIGHT = mLoginBox.getHeight();
+        ViewGroup.LayoutParams layout = mLoginBox.getLayoutParams();
+        layout.height=0;
+        mLoginBox.setLayoutParams(layout);
 
+        ValueAnimator sizeAnimator = ValueAnimator.ofInt(0, BOX_HEIGHT);
+        sizeAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mLoginBox.getLayoutParams().height = (int) valueAnimator.getAnimatedValue();
+                mLoginBox.requestLayout();
+            }
+        });
+        sizeAnimator.setDuration(1000);
+        sizeAnimator.start();
+//        start(Login.class);
     }
 
     @Override
-    public void onAnimationEnd(Animation animation) {
-        start(Login.class);
+    public void onAnimationCancel(Animator animator) {
     }
 
     @Override
-    public void onAnimationRepeat(Animation animation) {
-
+    public void onAnimationRepeat(Animator animator) {
     }
 }
